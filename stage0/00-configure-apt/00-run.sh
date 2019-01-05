f8c5b9864eb444fd027f0d777ec530630a04f616
Daniel Haus <daniel@deb-playground.de>
2019-01-05 12:39:16 +0100

    Fixed typo in package name

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -18,7 +18,7 @@ on_chroot apt-key add - < files/docker.gpg.key
 on_chroot << EOF
 apt-get update
 apt-get dist-upgrade -y
-apt-get install apt-transport-https ca-certificate gnupg2 software-properties-common -y
+apt-get install apt-transport-https ca-certificates gnupg2 software-properties-common -y
 EOF
 on_chroot << EOF
 sed -i -e 's/#//g' /etc/apt/sources.list
